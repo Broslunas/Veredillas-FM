312346e748211e5a166e61072278c71ac4343c45
Broslunas <pablo@broslunas.com>
2026-01-13 18:26:32 +0000

    add sitemap

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,42 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const episodios = await getCollection('episodios');
+  const blog = await getCollection('blog');
+  
+  // Combine and sort all content by date
+  const allContent = [
+    ...episodios.map(ep => ({
+      title: ep.data.title,
+      description: ep.data.description,
+      pubDate: ep.data.pubDate,
+      link: `/ep/${ep.slug}/`,
+      author: ep.data.author,
+      type: 'episode'
+    })),
+    ...blog.map(post => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`,
+      author: post.data.author,
+      type: 'post'
+    }))
+  ].sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+
+  return rss({
+    title: 'Veredillas FM',
+    description: 'El podcast oficial de Veredillas. Donde te mantenemos al pendiente de los temas más candentes.',
+    site: context.site,
+    items: allContent.map(item => ({
+      title: item.title,
+      description: item.description,
+      pubDate: item.pubDate,
+      link: item.link,
+      author: item.author,
+      categories: item.type === 'episode' ? ['Podcast', 'Episodio'] : ['Blog'],
+    })),
+    customData: `<language>es-ES</language>`,
+  });
+}
